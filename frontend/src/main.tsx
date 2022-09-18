f363e0f0937f7a9dfdeea672d25ce4ca2539222a
yamachu <yamachu.dev@gmail.com>
2022-09-18 16:20:38 +0900

    add react query provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <></>
+    <QueryClientProvider client={queryClient}>
+      <></>
+    </QueryClientProvider>
   </React.StrictMode>
 );
