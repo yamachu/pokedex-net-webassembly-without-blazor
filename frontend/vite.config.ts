789d7ab5684852adcd4c75cc14aff31ed97107be
yamachu <yamachu.dev@gmail.com>
2022-09-18 23:38:23 +0900

    add base path setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   define: {
     "process.env.POKEDEX_DOTNET_RUNTIME": JSON.stringify(
