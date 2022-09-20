01a4b713feda010e72aa02dd09cb9ae2edb92fd5
yamachu <yamachu.dev@gmail.com>
2022-09-21 00:12:52 +0900

    Add note

--- dotnet/src/Shared/src/PokedexUsecase.cs
+++ dotnet/src/Shared/src/PokedexUsecase.cs
@@ -14,11 +14,32 @@ public class QueryPokemon
         return dbHelper.AsyncBindConnection(
             async (c) =>
             {
-                // FIXME: if passed parameters to QueryAsync, Query, and more operations, it will crash
-                // ManagedError: Error occurred during a cryptographic operation.
-                // FIXME: if passed type argument for mapping return values, it will crash
-                // ManagedError: Error occurred during a cryptographic operation.
                 var result = await c.QueryAsync("select id, name from pokemons");
+                var mapped = result.OfType<IDictionary<string, object>>().Select(v =>
+                {
+                    return new Pokemon(
+                        Convert.ToInt32(v["id"]),
+                        Convert.ToString(v["name"])
+                    );
+                });
+
+                return mapped.ToArray();
+            },
+            Task.FromException<Pokemon[]>(new Exception("Cannot fetch pokemons"))
+        );
+    }
+
+    #region DO NOT USE ON BROWSER-WASM
+    // FIXME: if passed parameters to QueryAsync, Query, and more operations, it will crash
+    // ManagedError: Error occurred during a cryptographic operation.
+    // FIXME: if passed type argument for mapping return values, it will crash
+    // ManagedError: Error occurred during a cryptographic operation.
+    public static Task<Pokemon[]> FetchPokemons(SqliteHelper dbHelper, string query)
+    {
+        return dbHelper.AsyncBindConnection(
+            async (c) =>
+            {
+                var result = await c.QueryAsync("select id, name from pokemons where id = @query or name like @query", new { query });
                 var mapped = result.OfType<IDictionary<string, object>>().Select(v =>
                 {
                     return new Pokemon(
@@ -56,4 +77,5 @@ public class QueryPokemon
             Task.FromException<Pokemon[]>(new Exception("Cannot fetch pokemons"))
         );
     }
+    #endregion
 }
