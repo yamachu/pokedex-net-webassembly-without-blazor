062d77594f6791758ae46cfb20781822a7d38992
yamachu <yamachu.dev@gmail.com>
2022-09-21 23:14:03 +0900

    Add sample test

---
+++ dotnet/src/JavaScript/test/src/ConnectionTest.test.ts
@@ -0,0 +1,65 @@
+import { type RuntimeAPI } from "@microsoft/dotnet-runtime";
+import {
+  getTypedAssemblyExports,
+  setTypedModuleImports,
+} from "dotnet-webassembly-type-helper";
+import { beforeEach, describe, expect, it as _it, TestAPI } from "vitest";
+
+describe("ConnectionTest", () => {
+  interface LocalDotnetRuntimeContext {
+    runtimeAPI: RuntimeAPI;
+  }
+
+  const it = _it as TestAPI<LocalDotnetRuntimeContext>;
+
+  // NOTE: For ignoring to share state between each test case
+  let newDotnetRuntimeKey = 0;
+
+  beforeEach<LocalDotnetRuntimeContext>(async (context) => {
+    const m = `../node_modules/@microsoft/dotnet-runtime/dotnet.js?${newDotnetRuntimeKey++}`;
+    const { dotnet, exit } = await import(m).then(
+      (v: typeof import("@microsoft/dotnet-runtime")) => v
+    );
+    const runtimeAPI = await dotnet.create();
+
+    context.runtimeAPI = runtimeAPI;
+
+    return () => {
+      exit(0);
+      context.runtimeAPI = null as any as RuntimeAPI;
+    };
+  });
+
+  it(`return 1`, async (ctx) => {
+    const { getAssemblyExports, setModuleImports, getConfig } = ctx.runtimeAPI;
+
+    const config = getConfig();
+    const exported = await getTypedAssemblyExports(
+      getAssemblyExports(config.mainAssemblyName!)
+    );
+    setTypedModuleImports(setModuleImports, "main.mjs", {
+      sqlite: {
+        connection: () => "Data Source=:memory:",
+      },
+    });
+
+    exported.MyClass.Initialize();
+    expect(await exported.MyClass.ConnectionTest()).toBe(1);
+  });
+
+  it("return null when without Initialize", async (ctx) => {
+    const { getAssemblyExports, setModuleImports, getConfig } = ctx.runtimeAPI;
+
+    const config = getConfig();
+    const exported = await getTypedAssemblyExports(
+      getAssemblyExports(config.mainAssemblyName!)
+    );
+    setTypedModuleImports(setModuleImports, "main.mjs", {
+      sqlite: {
+        connection: () => "Data Source=:memory:",
+      },
+    });
+
+    expect(await exported.MyClass.ConnectionTest()).toBe(null);
+  });
+});
