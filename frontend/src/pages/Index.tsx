f21537ea9f7edf971a6f37707d0ced0eb22606a7
yamachu <yamachu.dev@gmail.com>
2022-09-25 18:39:51 +0900

    Add index

---
+++ frontend/src/pages/Index.tsx
@@ -0,0 +1,39 @@
+import { type FC } from "react";
+import { Link } from "react-router-dom";
+import { githubRepositoryUrl } from "../constant";
+
+export const Index: FC = () => {
+  return (
+    <div>
+      <h2>.NET JavaScript interop on WebAssembly Demonstration</h2>
+      <p>You can try this technology by two contents</p>
+
+      <ul>
+        <li style={ContentLiStyle}>
+          <Link to={"/basic"}>Basic</Link>
+          <span style={{ padding: "0 8px" }}>/</span>Fetch some data from own
+          dotnet library and list them
+        </li>
+        <li style={ContentLiStyle}>
+          <Link to={"/"}>Advanced</Link>
+          <span style={{ padding: "0 8px" }}>/</span>Multi runtime (WIP)
+        </li>
+      </ul>
+
+      <p>
+        Interested?
+        <a
+          href={githubRepositoryUrl}
+          target={"_blank"}
+          style={{ padding: "0 8px" }}
+        >
+          Go Repository!
+        </a>
+      </p>
+    </div>
+  );
+};
+
+const ContentLiStyle: React.CSSProperties = {
+  padding: "4px 0",
+};
